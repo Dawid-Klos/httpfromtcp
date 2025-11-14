21ef0aa654dcd4a22c321b1647e3750738d45674
Dawid Klos <klosdawid12@gmail.com>
2025-11-14 08:19:03 +0000

    feat: print headers from incoming request

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -30,6 +30,10 @@ func main() {
 		fmt.Printf("- Method: %s\n", req.RequestLine.Method)
 		fmt.Printf("- Target: %s\n", req.RequestLine.Target)
 		fmt.Printf("- Version: %s\n", req.RequestLine.HTTPVersion)
+		fmt.Println("Headers:")
+		for key, value := range req.Headers {
+			fmt.Printf("- %s: %s\n", key, value)
+		}
 
 		fmt.Println("-> Connection to", conn.RemoteAddr(), "closed")
 	}
